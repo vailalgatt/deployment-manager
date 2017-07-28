aafbe8cc9ec10dc27b8cefc14815d77ef3cfd08c
Vail Algatt <vail.algatt@gmail.com>
2017-07-27 21:11:42 -0600

    added init file

---
+++ app/init.js
@@ -0,0 +1,9 @@
+var app = {
+  controllers:{
+    weatherController: new WeatherController(),
+    imageController: new ImageController(),
+    quoteController: new QuoteController(),
+    todoController: new TodoController(),
+    clockControler: new ClockController()
+    }
+}
